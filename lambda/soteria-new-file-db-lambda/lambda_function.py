7dcdf728e5a88c39b9a2cd2ab6e524d6a9156d18
Nastia Felt <nfelt@ss-MacBook-Air.local>
2023-09-08 11:40:54 -0600

    add images to home page

--- lambda/soteria-new-file-db-lambda/lambda_function.py
+++ lambda/soteria-new-file-db-lambda/lambda_function.py
@@ -1,5 +1,3 @@
-
-
 import urllib.parse
 import boto3
 import hashlib
@@ -27,6 +25,7 @@ def lambda_handler(event, context):
         response = s3.get_object(Bucket=bucket, Key=key)
         
         print("key: " + key)
+        
         if (re.search(r'\.(jpe?g|png)$', key) and not
             re.search(r'-([0-9]{1,6}x[0-9]{1,6}|scaled)\.(jpe?g|png)$', key)):
             
@@ -39,7 +38,7 @@ def lambda_handler(event, context):
             s3.put_object(Bucket=bucket, Key=new_key, Body=image_hash)
             
             
-            # MySQL connection detail 
+            # MySQL connection details
             # Connect to MySQL
             connection = pymysql.connect(
                 host = 'ls-eb80f01475f58c75e5a6a7fb7198a542bc959961.c6sougbexbsi.us-west-2.rds.amazonaws.com',
@@ -52,7 +51,7 @@ def lambda_handler(event, context):
             cursor = connection.cursor()
         
             ## find the post_id for this image in wordpress
-            select_query = "SELECT ID FROM wp_posts WHERE post_type = 'attachment' AND guid LIKE %s"
+            select_query = "SELECT ID FROM wp_posts WHERE post_type = 'attachment' AND guid LIKE %s;"
             cursor.execute(select_query, ('%' + key))
             result_set = cursor.fetchall()
             post_id = result_set[0][0]
@@ -61,20 +60,44 @@ def lambda_handler(event, context):
             check_query = "SELECT * FROM wp_postmeta WHERE post_id = %s AND meta_key = 'custom_hash' AND meta_value = %s;"
             cursor.execute(check_query, (post_id, image_hash))
             existing_hash = cursor.fetchall()
+
             
             # If the image hash doesn't exist, insert it
             if not existing_hash:
                 insert_query = "INSERT INTO wp_postmeta (post_id, meta_key, meta_value) VALUES (%s, 'custom_hash', %s);"
                 cursor.execute(insert_query, (post_id, image_hash))
+
+                ## add image to home page
+                
+                # get the post that represents the home page
+                magic_home_post_id=472
+                
+                # get the content for the home page post
+                add_img_query = "SELECT post_content FROM wp_posts WHERE ID=%s"
+                cursor.execute(add_img_query, (magic_home_post_id))
+                home_post_content = cursor.fetchall()[0][0]
+               
+                # regex reference
+                # gallery_ids="1043,1037,1036,1035,1034"
+                # r'gallery_ids="[0-9,]+"',
+
+                # replace the existing list of gallery_ids (images) with 
+                # the same list plus the id of the new image
+                new_home_post_content = re.sub(
+                        r'(gallery_ids="[0-9,]+)"',
+                        r"\1," + str(post_id) + r'"',
+                        home_post_content
+                        )
+                
+                # update the actual home page post with the new post_content
+                update_home_query = "UPDATE wp_posts SET post_content=%s WHERE ID=%s"
+                cursor.execute(update_home_query, (new_home_post_content, magic_home_post_id))
             
             # Commit the changes
             connection.commit()
 
-            
             return new_key
 
-        # return (key)
-        
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
